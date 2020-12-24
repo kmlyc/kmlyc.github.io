91762c30f52c210d86ec5c8f9019118cf8159877
kmlyc <826644070@qq.com>
2020-12-24 21:57:02 +0800

    Update love.js

--- file/love.js
+++ file/love.js
@@ -168,7 +168,7 @@
 
             ctx.moveTo(0, 0);
             ctx.scale(0.75, 0.75);
-            ctx.font = "20px Verdana"; // 字号肿么没有用? (ˉ(∞)ˉ)
+            ctx.font = "15px Verdana"; // 字号肿么没有用? (ˉ(∞)ˉ)
             ctx.fillText("Come Baby", 23, 10);
             ctx.restore();
         },
